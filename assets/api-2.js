f8f48e3fd9b9dca7bb0c522af3f08e8f7c055aa2
Lawson Vanderpool <lawsonvanderpool@gmail.com>
2022-03-29 13:11:26 -0700

    added event listener function and printResult function

---
+++ assets/api-2.js
@@ -0,0 +1,15 @@
+let resultContentEl = document.querySelector('#result-content');
+
+function printResults(resultObj) {
+    console.log(resultObj);
+    let endCard = document.createElement("div");
+
+    let endBody = document.createElement("div");
+    endCard.append(endBody);
+
+    let titleEL = document.createElement("h3");
+    titleEL.textContent = resultObj.title;
+
+    let bodyContentEl = document.createElement("p");
+   // bodyContentEl.innerHTML = 
+}
