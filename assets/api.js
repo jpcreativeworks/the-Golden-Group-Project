01a4d6cee68bf493be0b395ab3451176d261e7c1
Brant Martin <brantmartin473@gmail.com>
2022-03-30 12:09:34 -0700

    got the base for fetching the api's that we need

--- assets/api.js
+++ assets/api.js
@@ -1,8 +1,12 @@
-//let titleSearch = document.querySelector(#placeholder)
-//let actorSearch = document.querySelector(#placeholder)
-//let genreSelect = document.querySelector(#placeholder)
-//let ratingSelect = document.querySelector(#placeholder)
-//let searchBox = document.querySelector(#placeholder)
+//I want this Javascript to take the search parameters from the URL
+//and search through the IMDB-API and place the results into the
+//HTML as information that can be displayed for the user to see
+
+let titleSearch = document.querySelector('#title')
+let actorSearch = document.querySelector('#actor')
+let genreSelect = document.querySelector('#genre')
+let ratingSelect = document.querySelector('#ratings')
+let searchBox = document.querySelector('#form')
 
 function getSelect() {
     let searchSelectArr = document.location.search.split('&');
@@ -13,22 +17,38 @@ function getSelect() {
     searchMovies(titleVal, actorVal, genreVal, searchVal);
 };
 
-function searchMovies(titleVal, actorVal, genreVal, searchVal) {
-    
-}
+// function searchMovies(titleVal, actorVal, genreVal, searchVal) {
+// }
 
 const options = {
-    method: 'GET',
-    headers: {
-        'X-RapidAPI-Host': 'data-imdb1.p.rapidapi.com',
-        'X-RapidAPI-Key': '75dcd5e991mshf216fa4ad81e4abp12c569jsne095f7b6e197'
-    }
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Host': 'data-imdb1.p.rapidapi.com',
+		'X-RapidAPI-Key': '75dcd5e991mshf216fa4ad81e4abp12c569jsne095f7b6e197'
+	}
 };
 
-fetch('https://data-imdb1.p.rapidapi.com', options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    
-    .catch(err => console.error(err))
-    
-    
+{fetch ('https://data-imdb1.p.rapidapi.com/titles/search/title/%7Btitle%7D?info=mini_info&limit=10&page=1&titleType=movie', options)
+	.then(responseTitle => responseTitle.json())
+	.then(responseTitle => console.log(responseTitle))
+	.catch(err => console.error(err))
+}
+
+{fetch('https://data-imdb1.p.rapidapi.com/actors?limit=10&page=1', options)
+.then(responseActor => responseActor.json())
+.then(responseActor => console.log(responseActor))
+.catch(err => console.error(err));
+}
+
+{fetch('https://data-imdb1.p.rapidapi.com/titles/tt0000002/ratings', options)
+	.then(responseRating => responseRating.json())
+	.then(responseRating => console.log(responseRating))
+	.catch(err => console.error(err));
+}
+
+{fetch('https://data-imdb1.p.rapidapi.com/titles/utils/genres', options)
+	.then(responseGenre => responseGenre.json())
+	.then(responseGenre => console.log(responseGenre))
+	.catch(err => console.error(err));
+}
+
