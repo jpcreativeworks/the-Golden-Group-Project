ce8935943a006eebe98ef8600089eb1a9565814a
Lawson Vanderpool <lawsonvanderpool@gmail.com>
2022-03-31 13:54:59 -0700

    started working on fetch functions/event listeners

--- assets/api-2.js
+++ assets/api-2.js
@@ -1,73 +1,73 @@
-let titleSubmitButton = document.querySelector("#title-search");
-let actorSubmitButton = document.querySelector("#actor-search");
-let ratingSubmitButton = document.querySelector("#rating-search");
-let genreSubmitButton = document.querySelector("#genre-search");
-let resultContent = document.querySelector("#result-content");
+// let titleSubmitButton = document.querySelector("#title-search");
+// let actorSubmitButton = document.querySelector("#actor-search");
+// let ratingSubmitButton = document.querySelector("#rating-search");
+// let genreSubmitButton = document.querySelector("#genre-search");
+// let resultContent = document.querySelector("#result-content");
 
-function printResults() {
-    let resultsTitle = document.querySelector("#results-title")
-    let resultsActor = document.querySelector("#results-actor")
-    let resultsRating = document.querySelector("#results-rating")
-    let resultsGenre = document.querySelector("#results-title")
-    let resultsDescription = document.querySelector("#results-discription")
-    let titleInputVal = document.querySelector('#title').value;
+// function printResults() {
+//     let resultsTitle = document.querySelector("#results-title")
+//     let resultsActor = document.querySelector("#results-actor")
+//     let resultsRating = document.querySelector("#results-rating")
+//     let resultsGenre = document.querySelector("#results-title")
+//     let resultsDescription = document.querySelector("#results-discription")
+//     let titleInputVal = document.querySelector('#title').value;
 
-    resultsTitle.textContent = titleInputVal
+//     resultsTitle.textContent = titleInputVal
 
-}
+// }
 
-function getParams() {
+// function getParams() {
     
-    let searchParamsArr = document.location.search.split('=').pop();
-    let titleInput = document.querySelector('#title').value;
-    //let title = searchParamsArr.split('=').pop();
-    titleInput.textContent = searchParamsArr
-    console.log(searchParamsArr)
-  }
+//     let searchParamsArr = document.location.search.split('=').pop();
+//     let titleInput = document.querySelector('#title').value;
+//     //let title = searchParamsArr.split('=').pop();
+//     titleInput.textContent = searchParamsArr
+//     console.log(searchParamsArr)
+//   }
 
-function titleSearchFormSubmit(event) {
-   // event.preventDefault();
-    let titleInputVal = document.querySelector('#title').value;
+// function titleSearchFormSubmit(event) {
+//    // event.preventDefault();
+//     let titleInputVal = document.querySelector('#title').value;
 
-    let queryString = "./results.html?q=" + titleInputVal;
-    location.assign(queryString);
+//     let queryString = "./results.html?q=" + titleInputVal;
+//     location.assign(queryString);
 
-}
-titleSubmitButton.onclick = function() {
-    titleSearchFormSubmit();
-    getParams()
-}
-
-
-
-function actorSearchFormSubmit(event) {
-    event.preventDefault();
-    let actorInputVal = document.querySelector('#actor').value;
-
-    let queryString = "./results.html?q=" + actorInputVal;
-
-    location.assign(queryString);
-}
-actorSubmitButton.onclick = actorSearchFormSubmit
+// }
+// titleSubmitButton.onclick = function() {
+//     titleSearchFormSubmit();
+//     getParams()
+// }
 
 
-function ratingSearchFormSubmit(event) {
-    event.preventDefault();
-    let ratingInputVal = document.querySelector('#ratings').value;
 
-    let queryString = "./results.html?q=" + ratingInputVal;
+// function actorSearchFormSubmit(event) {
+//     event.preventDefault();
+//     let actorInputVal = document.querySelector('#actor').value;
 
-    location.assign(queryString);
-}
-ratingSubmitButton.onclick = ratingSearchFormSubmit;
+//     let queryString = "./results.html?q=" + actorInputVal;
+
+//     location.assign(queryString);
+// }
+// actorSubmitButton.onclick = actorSearchFormSubmit
 
 
-function genreSearchFormSubmit(event) {
-    event.preventDefault();
-    let genreInputVal = document.querySelector('#genre').value;
+// function ratingSearchFormSubmit(event) {
+//     event.preventDefault();
+//     let ratingInputVal = document.querySelector('#ratings').value;
 
-    let queryString = "./results.html?q=" + genreInputVal;
+//     let queryString = "./results.html?q=" + ratingInputVal;
 
-    location.assign(queryString);
-}
-genreSubmitButton.onclick = genreSearchFormSubmit
+//     location.assign(queryString);
+// }
+// ratingSubmitButton.onclick = ratingSearchFormSubmit;
+
+
+// function genreSearchFormSubmit(event) {
+//     event.preventDefault();
+//     let genreInputVal = document.querySelector('#genre').value;
+
+//     let queryString = "./results.html?q=" + genreInputVal;
+
+//     location.assign(queryString);
+// }
+// genreSubmitButton.onclick = genreSearchFormSubmit
--- assets/api.js
+++ assets/api.js
@@ -1,73 +1,5 @@
 //We want to display movies on the search screen based on the info that we get when we search the json that the user is choosing to search
 
-let titleSearch = document.querySelector('#title')
-let actorSearch = document.querySelector('#actor')
-let genreSelect = document.querySelector('#genre')
-let ratingSelect = document.querySelector('#ratings')
-let searchBox = document.querySelector('#form')
-
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Host': 'data-imdb1.p.rapidapi.com',
-		'X-RapidAPI-Key': '75dcd5e991mshf216fa4ad81e4abp12c569jsne095f7b6e197'
-	}
-};
-
-titleUrl = 'https://data-imdb1.p.rapidapi.com/titles/search/keyword/%7Bkeyword%7D?info=mini_info&limit=10&page=1&titleType=movie'
-actorUrl = 'https://data-imdb1.p.rapidapi.com/actors?limit=10&page=1'
-ratingUrl = 'https://data-imdb1.p.rapidapi.com/titles/tt0000002/ratings'
-genreUrl = 'https://data-imdb1.p.rapidapi.com/ts/utilitles/genres'
-
-function getTitleInfo(titleSearch) {
-	let titleUrl = 'https://data-imdb1.p.rapidapi.com/titles/search/keyword/' + titleSearch + '?info=mini_info&limit=10&page=1&titleType=movie'
-	fetch(titleUrl, options)
-		.then(responseTitle => responseTitle.json())
-		.then(responseTitle => {
-			localStorage.setItem('titleInfo', JSON.stringify(responseTitle))
-			console.log(responseTitle)
-		})
-		.catch(err => console.error(err))
-}
-
-function getActorInfo() {
-	let actorUrl = 'https://data-imdb1.p.rapidapi.com/actors/' + actorSearch + '?limit=10&page=1'
-	fetch(actorUrl, options)
-		.then(responseActor => responseActor.json())
-		.then(responseActor => {
-			localStorage.setItem('actorInfo', JSON.stringify(responseActor))
-			console.log(responseActor)
-		})
-		.catch(err => console.error(err));
-}
-
-function getRatingInfo() {
-	let ratingUrl = 'https://data-imdb1.p.rapidapi.com/titles/' + ratingSearch + '/ratings'
-	fetch(ratingUrl, options)
-		.then(responseRating => responseRating.json())
-		.then(responseRating => {
-			localStorage.setItem('ratingInfo', JSON.stringify(responseRating))
-			console.log(responseRating)
-		})
-		.catch(err => console.error(err));
-}
-
-function getGenreInfo() {
-	let genreUrl = 'https://data-imdb1.p.rapidapi.com/ts/utilitles/genres/' + genreSearch
-	fetch(genreUrl, options)
-		.then(responseGenre => responseGenre.json())
-		.then(responseGenre => {
-			localStorage.setItem('genreInfo', JSON.stringify(responseGenre))
-			console.log(responseGenre)
-		})
-		.catch(err => console.error(err));
-}
-
-getTitleInfo()
-getActorInfo()
-getRatingInfo()
-getGenreInfo()
-
 let titleSubmitButton = document.querySelector("#title-search");
 let actorSubmitButton = document.querySelector("#actor-search");
 let ratingSubmitButton = document.querySelector("#rating-search");
@@ -82,16 +14,14 @@ let titleInputVal = document.querySelector('#title').value;
 
 function printResults() {
 	let searchParamsArr = document.location.search.split('=').pop();
-    let titlePTag = document.createElement("p");
-	titlePTag.textContent = searchParamsArr;
-	resultsTitle.append(titlePTag)
+    
 }
 
-function getParams() {
+// function getParams() {
     
-    let searchParamsArr = document.location.search.split('=').pop();
+//     let searchParamsArr = document.location.search.split('=').pop();
     
-  }
+//   }
 
 function titleSearchFormSubmit() {
 	
@@ -101,16 +31,17 @@ function titleSearchFormSubmit() {
     location.assign(queryString);
 	printResults()
 }
-titleSubmitButton.onclick = function(event) {
-	event.preventDefault();
+titleSubmitButton.onclick = function() {
+	
 	let titleInputVal = document.querySelector('#title').value;
  console.log(titleInputVal)
       let queryString = "./results.html?q=" + titleInputVal;
       location.assign(queryString);
+	  
 }
 
-printResults()
-getParams()
+ printResults()
+// getParams()
 
 function actorSearchFormSubmit(event) {
     event.preventDefault();
@@ -143,3 +74,74 @@ function genreSearchFormSubmit(event) {
     location.assign(queryString);
 }
 genreSubmitButton.onclick = genreSearchFormSubmit
+
+
+
+let actorSearch = document.querySelector('#actor')
+let genreSelect = document.querySelector('#genre')
+let ratingSelect = document.querySelector('#ratings')
+let searchBox = document.querySelector('#form')
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Host': 'data-imdb1.p.rapidapi.com',
+		'X-RapidAPI-Key': '75dcd5e991mshf216fa4ad81e4abp12c569jsne095f7b6e197'
+	}
+};
+
+titleUrl = 'https://data-imdb1.p.rapidapi.com/titles/search/keyword/%7Bkeyword%7D?info=mini_info&limit=10&page=1&titleType=movie'
+actorUrl = 'https://data-imdb1.p.rapidapi.com/actors?limit=10&page=1'
+ratingUrl = 'https://data-imdb1.p.rapidapi.com/titles/tt0000002/ratings'
+genreUrl = 'https://data-imdb1.p.rapidapi.com/ts/utilitles/genres'
+
+function getTitleInfo() {
+	let titleSearch = document.location.search.split('=').pop();
+	console.log(titleSearch)
+	let titleUrl = 'https://data-imdb1.p.rapidapi.com/titles/search/keyword/' + titleSearch + '?info=mini_info&limit=10&page=1&titleType=movie'
+	fetch(titleUrl, options)
+		.then(responseTitle => responseTitle.json())
+		.then(responseTitle => {
+			localStorage.setItem('titleInfo', JSON.stringify(responseTitle))
+			console.log(responseTitle)
+		})
+		.catch(err => console.error(err))
+}
+
+function getActorInfo() {
+	let actorUrl = 'https://data-imdb1.p.rapidapi.com/actors/' + actorSearch + '?limit=10&page=1'
+	fetch(actorUrl, options)
+		.then(responseActor => responseActor.json())
+		.then(responseActor => {
+			localStorage.setItem('actorInfo', JSON.stringify(responseActor))
+			console.log(responseActor)
+		})
+		.catch(err => console.error(err));
+}
+
+function getRatingInfo() {
+	let ratingUrl = 'https://data-imdb1.p.rapidapi.com/titles/' + ratingSelect + '/ratings'
+	fetch(ratingUrl, options)
+		.then(responseRating => responseRating.json())
+		.then(responseRating => {
+			localStorage.setItem('ratingInfo', JSON.stringify(responseRating))
+			console.log(responseRating)
+		})
+		.catch(err => console.error(err));
+}
+
+function getGenreInfo() {
+	let genreUrl = 'https://data-imdb1.p.rapidapi.com/ts/utilitles/genres/' + genreSelect
+	fetch(genreUrl, options)
+		.then(responseGenre => responseGenre.json())
+		.then(responseGenre => {
+			localStorage.setItem('genreInfo', JSON.stringify(responseGenre))
+			console.log(responseGenre)
+		})
+		.catch(err => console.error(err));
+}
+
+// getGenreInfo();
+// getRatingInfo();
+ getTitleInfo()
+// getActorInfo();
