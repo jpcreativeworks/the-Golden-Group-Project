df2215eaa8a5da2c2394a7e640fdfb159b7e1991
Lawson Vanderpool <lawsonvanderpool@gmail.com>
2022-03-30 21:37:43 -0700

    added event listeners to the rest of the search buttons

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,9 @@ let actorInputVal = document.querySelector('#actor').value;
 let genreInputVal = document.querySelector("#genre").value;
 let ratingInputVal = document.querySelector("#ratings").value;
 let titleSubmitButton = document.querySelector("#title-search")
+let actorSubmitButton = document.querySelector("#actor-search")
+let ratingSubmitButton = document.querySelector("#rating-search")
+let genreSubmitButton = document.querySelector("#genre-search")
 
 // function handleSearchFormSubmit(event) {
 //   event.preventDefault();
@@ -27,13 +30,40 @@ let titleSubmitButton = document.querySelector("#title-search")
 
 function titleSearchFormSubmit(event) {
   event.preventDefault();
-
   let titleInputVal = document.querySelector('#title').value;
-  
+
   let queryString = "./results.html?q=" + titleInputVal;
 
   location.assign(queryString);
 }
-
-
 titleSubmitButton.onclick = titleSearchFormSubmit
+
+function actorSearchFormSubmit(event) {
+  event.preventDefault();
+  let actorInputVal = document.querySelector('#actor').value;
+
+  let queryString = "./results.html?q=" + actorInputVal;
+
+  location.assign(queryString);
+}
+actorSubmitButton.onclick = actorSearchFormSubmit
+
+function ratingSearchFormSubmit(event) {
+  event.preventDefault();
+  let ratingInputVal = document.querySelector('#ratings').value;
+
+  let queryString = "./results.html?q=" + ratingInputVal;
+
+  location.assign(queryString);
+}
+ratingSubmitButton.onclick = ratingSearchFormSubmit
+
+function genreSearchFormSubmit(event) {
+  event.preventDefault();
+  let genreInputVal = document.querySelector('#genre').value;
+
+  let queryString = "./results.html?q=" + genreInputVal;
+
+  location.assign(queryString);
+}
+genreSubmitButton.onclick = genreSearchFormSubmit
